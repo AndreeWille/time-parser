dc649eed86432a4197590a063f574826b8cc3742
Andree Wille <AndreeWille@users.noreply.github.com>
2020-01-12 15:11:23 +0100

    generate proecjt structure with stagehand

---
+++ example/time_parser_example.dart
@@ -0,0 +1,6 @@
+import 'package:time_parser/time_parser.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/time_parser.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library time_parser;
+
+export 'src/time_parser_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
